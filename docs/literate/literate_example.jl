1b11a0f8276be5e58d89e4f1318b3f319b41e1b7
James Fairbanks <fairbanksj@ufl.edu>
2023-08-16 20:59:59 -0400

    DOC: use the docs template

--- docs/literate/literate_example.jl
+++ docs/literate/literate_example.jl
@@ -6,8 +6,132 @@
 
 using SyntacticModels
 
-# ## Using `hello()`
-#
-# We provide the `hello(string)` method which prints "Hello, `string`!"
+using SyntacticModels.AMR
+using SyntacticModels.ASKEMDecapodes
+using SyntacticModels.ASKEMUWDs
+using SyntacticModels.Composites
 
-hello("World")
+using MLStyle
+using Decapodes
+using Catlab
+using Catlab.RelationalPrograms
+using Catlab.WiringDiagrams
+using Test
+using JSON3
+
+
+
+x = Typed(:X, :Form0)
+v = Typed(:V, :Form0)
+Q = Typed(:Q, :Form0)
+
+c = [x, Q]
+s = [Statement(:oscillator, [x,v]),
+  Statement(:heating, [v,Q])]
+u = ASKEMUWDs.UWDExpr(c, s)
+
+
+
+h = AMR.Header("harmonic_oscillator",
+  "modelreps.io/DecaExpr",
+  "A Simple Harmonic Oscillator as a Diagrammatic Equation",
+  "DecaExpr",
+  "v1.0")
+
+# The easiest way to write down a DecaExpr is in our DSL and calling the parser.
+dexpr = Decapodes.parse_decapode(quote
+  X::Form0{Point}
+  V::Form0{Point}
+
+  k::Constant{Point}
+
+  ∂ₜ(X) == V
+  ∂ₜ(V) == -1*k*(X)
+end
+)
+
+# That gave us the first model
+d1 = ASKEMDecaExpr(h, dexpr)
+
+# The second model is:
+d2 = ASKEMDecaExpr(
+  AMR.Header("fricative_heating",
+   "modelreps.io/SummationDecapode",
+   "Velocity makes it get hot, but you dissipate heat away from Q₀",
+   "SummationDecapode", "v1.0"),
+    Decapodes.parse_decapode(quote
+      V::Form0{Point}
+      Q::Form0{Point}
+      κ::Constant{Point}
+      λ::Constant{Point}
+      Q₀::Parameter{Point}
+
+      ∂ₜ(Q) == κ*V + λ(Q - Q₀)
+    end)
+)
+
+# Now we can assemble this bad boi:
+h = AMR.Header("composite_physics", "modelreps.io/Composite", "A composite model", "CompositeModelExpr", "v0.0")
+m = CompositeModelExpr(h, u, [OpenModel(d1, [:X, :Ẋ]), OpenModel(d2, [:V, :Q])])
+interface(m) == [:X, :Q]
+JSON3.write(m) # you can see from this little model (two coupled odes even) that the jsons will not be human editable. 
+
+# now we can interpret this big data structure to execute a composition!
+composite = oapply(m)
+display(apex(composite))
+to_graphviz(apex(composite))
+
+
+# TESTING NESTED COMPOSITION
+
+Q₊ = Untyped(:Q₊)
+Q₋ = Untyped(:Q₋)
+Q̇ = Untyped(:Q̇)
+
+uwdʰ = UWDExpr([v, Q], [Statement(:drag, [v, Q₊]), Statement(:cooling, [Q₋, Q]), Statement(:superposition, [Q₊, Q₋, Q̇])])
+
+drag = ASKEMDecaExpr(
+  AMR.Header("DragHeat", "modelreps.io/SummationDecapode", "velocity makes it get hot", "SummationDecapode", "v1.0"),
+  Decapodes.parse_decapode(quote
+    V::Form0{Point}
+    Q₊::Form0{Point}
+    κ::Constant{Point}
+
+    Q₊ == κ*V 
+  end)
+)
+
+cooling = ASKEMDecaExpr(
+  AMR.Header("NetwonCooling", "modelreps.io/SummationDecapode", "heat dissipates to the enviornment", "SummationDecapode", "v1.0"),
+  Decapodes.parse_decapode(quote
+    Q₋::Form0{Point}
+    Q₀::Parameter{Point}
+    Q::Form0{Point}
+    λ::Constant{Point}
+
+    Q₋ == λ(Q-Q₀)
+  end)
+)
+
+superposition = ASKEMDecaExpr(
+  AMR.Header("LinearSuperpositon", "modelreps.io/SummationDecapode", "variables be addin", "SummationDecapode", "v1.0"),
+  Decapodes.parse_decapode(quote
+    X::Form0{Point}
+    Y::Form0{Point}
+    T::Form0{Point}
+
+    T == X + Y
+  end)
+)
+
+h = AMR.Header("hierarchical_composite", "modelreps.io/Composite", "A hierarchical composite model of frictional heating", "CompositeModelExpr", "v0.1")
+m = CompositeModelExpr(h,u, [OpenModel(d1, [:X, :Ẋ]),
+      CompositeModelExpr(AMR.Header("heating_dynamics", "modelreps.io/Composite", "A formula for heating - cooling", "CompositeModelExpr", "v0.1"),
+        uwdʰ, [OpenModel(drag, [:V, :Q₊]), OpenModel(cooling, [:Q₋, :Q]), OpenModel(superposition, [:X, :Y, :T])])
+])
+
+dh = apex(oapply(m))
+
+composite = OpenDecapode(m)
+hf = composite.model.header
+ASKEMDecapode(Header("flattened_composite", hf.schema, "A flattened version of the composite_physics model.", hf.schema_name, hf.model_version), composite.model.model)
