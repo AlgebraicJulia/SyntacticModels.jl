f6e4db13f2d6e9596b49bc1833f901602b0e803c
p-stokes <patrick.stokes@gmail.com>
2023-12-05 15:33:39 -0500

    Started making intertype version for composite_models.

---
+++ src/composite_models_it.jl
@@ -0,0 +1,85 @@
+module Composites
+
+export CompositeModelExpr, OpenModel, OpenDecapode, CompositeModel, interface, open_decapode
+
+using MLStyle
+using Catlab
+# using Decapodes
+using StructTypes
+
+# using ..SyntacticModelsBase
+using ..AMR
+using ..ASKEMDecapodes
+using ..ASKEMUWDs
+
+using ACSets
+using ACSets.InterTypes
+
+using Reexport
+@reexport using MLStyle
+@reexport using ACSets
+using ACSets.ADTs
+using ACSets.ACSetInterface
+
+
+using ..AMR.amr
+using ..ASKEMDecapodes.decapodes
+using ..ASKEMUWDs.uwd
+
+@intertypes "composite_models.it" module composites
+  import ..amr
+  import ..decapodes
+  import ..uwd
+end
+
+using .composites
+
+"""    interface(m::CompositeModel)
+
+Extract the interface of a composite model. If the model is open, then it is the feet of the cospan. If it is a Composite, then it is the context of the uwd.
+"""
+interface(m::CompositeModel) = @match m begin
+  OpenModel(M, I) => I
+  CompositeModelExpr(h, uwd, components) => map(ASKEMUWDs.varname, context(uwd))
+end
+
+# Extract an open decapode from the decapode expression and the interface
+open_decapode(d, interface) = Open(SummationDecapode(d.model), interface)
+open_decapode(d::ASKEMDecaExpr, interface) = Open(SummationDecapode(d.model), interface)
+open_decapode(d::ASKEMDecapode, interface) = Open(d.model, interface)
+
+"""    Catlab.oapply(m::CompositeModel)
+
+CompositeModels can be flattened into a single level of model with the oapply function.
+
+!!! warning 
+    Because the oapply algorithm operates on the compute graph representation of the equations, it does not produce syntactic equations. 
+    Calls to oapply produce instances of OpenDecapode and not DecaExpr. 
+    Software that expects to consume decapodes should plan to interact with both forms.
+"""
+function Catlab.oapply(m::CompositeModel)
+  let ! = oapply
+    @match m begin
+      # For a primitive model we just attach the interface
+      OpenModel(M, I) => open_decapode(M,I)
+      OpenDecapode(M, I) => open_decapode(M,I)
+      # For a composite model, we have to recurse
+      CompositeModelExpr(h, pattern, components) => begin
+        uwd = ASKEMUWDs.construct(RelationDiagram, pattern)
+        Ms = map(m.components) do mᵢ; 
+          !(mᵢ) # oapply all the component models recursively
+        end
+        # OpenDecapode(ASKEMDecapode(h, apex(!(uwd, Ms))), interface(m)) # Then we call the oapply from Decapodes.
+        Open(apex(!(uwd, Ms)), uwd[[:outer_junction, :variable]]) # Then we call the oapply from Decapodes.
+      end
+    end
+  end
+end
+
+function OpenDecapode(m::CompositeModel)
+  composite = oapply(m)
+  feet = map(l->only(dom(l)[:name]), legs(composite))
+  OpenDecapode(ASKEMDecapode(m.header,apex(composite)), feet)
+end
+
+end
